badbacfbe556397e7fa71e8b001e66bff2465123
Dharsh <dharsh@Dharshs-MacBook-Air.local>
2026-05-25 00:52:35 +0530

    fix cron route deployment

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,84 +1,14 @@
 import { NextResponse } from "next/server";
-import { prisma } from "../../../lib/prisma";
+
+export const dynamic = "force-dynamic";
 
 export async function GET() {
 
   try {
 
-    // Get only expired pending reservations
-    const expiredReservations =
-      await prisma.reservation.findMany({
-        where: {
-          status: "PENDING",
-
-          expiresAt: {
-            lte: new Date(),
-          },
-        },
-      });
-
-    for (const reservation of expiredReservations) {
-
-      // Get inventory first
-      const inventory =
-        await prisma.inventory.findUnique({
-          where: {
-            productId_warehouseId: {
-              productId:
-                reservation.productId,
-
-              warehouseId:
-                reservation.warehouseId,
-            },
-          },
-        });
-
-      // Prevent negative values
-      if (!inventory) continue;
-
-      await prisma.inventory.update({
-        where: {
-          productId_warehouseId: {
-            productId:
-              reservation.productId,
-
-            warehouseId:
-              reservation.warehouseId,
-          },
-        },
-
-        data: {
-
-          // restore stock
-          stock: {
-            increment:
-              reservation.quantity,
-          },
-
-          // decrease reserved stock safely
-          reservedStock:
-            Math.max(
-              inventory.reservedStock -
-                reservation.quantity,
-              0
-            ),
-        },
-      });
-
-      // Update reservation status
-      await prisma.reservation.update({
-        where: {
-          id: reservation.id,
-        },
-
-        data: {
-          status: "EXPIRED",
-        },
-      });
-    }
-
     return NextResponse.json({
       success: true,
+      message: "Cron route working",
     });
 
   } catch (error) {
